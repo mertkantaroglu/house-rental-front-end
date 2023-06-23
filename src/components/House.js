f20b63a80b35d188f042789b17d53e7c67ec3d7a
Zeph180 <zephrichards1@gmail.com>
2023-06-23 16:05:56 +0300

    Setup routes

---
+++ src/components/House.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchHouse } from '../store/HouseSlice'
+
+const HousePage = () => {
+  const dispatch = useDispatch()
+  const house = useSelector((state) => state.house)
+  console.log(house)
+
+  useEffect(() => {
+    dispatch(fetchHouse())
+  }, [dispatch])
+
+  return (
+    <div>
+      <h1>House</h1>
+      <p>Name</p>
+      <p>Address</p>
+      <p>Price</p>
+    </div>
+  )
+}
+
+export default HousePage
