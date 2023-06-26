f380a6d29ed49fca52305eeed66ecbec14b91560
Zeph180 <zephrichards1@gmail.com>
2023-06-26 14:05:31 +0300

    Make navbar responsive

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Hamburger from "../assets/hamburg";
+import CloseBtn from "../assets/CloseBtn";
 import "./NavBar.css";
 
-const NavBar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Our Houses</Link>
-      </li>
-      <li>
-        <Link to="/house">House</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const NavBar = () => {
+  const [isListShown, setIsListShown] = useState(false);
+
+  const closeList = () => {
+    document.querySelector(".nav-list").classList.remove("visible");
+    setIsListShown(false);
+  }
+
+  const showList = () => {
+    document.querySelector(".nav-list").classList.add("visible");
+    setIsListShown(true);
+  }
+
+  return (
+    <nav className="nav-container">
+      <div className="nav-icons">
+        <h1>Royalty Houses</h1>
+        <section className='toggled-btns'>
+          {isListShown ? (
+            <CloseBtn onClick={closeList} />
+          ) : (
+            <Hamburger onClick={showList} />
+          )}
+        </section>
+      </div>
+        <ul className='nav-list'>
+          <li>
+            <Link to="/">Our Houses</Link>
+          </li>
+          <li>
+            <Link to="/house">House</Link>
+          </li>
+        </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
