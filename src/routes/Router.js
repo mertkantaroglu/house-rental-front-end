4538911f425db336e94029729b6d430c2f4eb275
LeslieAine <aineleslie@gmail.com>
2023-06-27 23:31:40 +0300

    Update routes

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../components/Homepage";
 import HousePage from "../components/House";
+import House from '../pages/House';
 
 const AppRouter = () => (
   <Routes>
     <Route path="/" element={<Homepage />} />
     <Route path="/house" element={<HousePage />} />
+    <Route path="/reservations" element={<House />} />
+    <Route path="/delete" element={<House />} />
   </Routes>
 );
 
