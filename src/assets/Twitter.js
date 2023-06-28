54facf36846786758c0934142668f0998d2d4013
Zeph180 <zephrichards1@gmail.com>
2023-06-28 11:28:14 +0300

    Create horizontal scroll

---
+++ src/assets/Twitter.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function TwitterIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#0F85F2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M23 3.01s-2.018 1.192-3.14 1.53a4.48 4.48 0 00-7.86 3v1a10.66 10.66 0 01-9-4.53s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5 0-.278-.028-.556-.08-.83C21.94 5.674 23 3.01 23 3.01z"
+      ></path>
+    </svg>
+  );
+}
+
+export default TwitterIcon;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,21 +1,139 @@
-import React, { useEffect} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchHouses } from '../store/HousesSlice'
+import React, { useEffect,useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchHouses } from "../store/HousesSlice";
+import defaultHouse from "../assets/defaultHouse.jpg";
+import "./Home.css";
+import MediumIcon from "../assets/Medium";
+import TwitterIcon from "../assets/Twitter";
+import LinkedInIcon from "../assets/LinkedIn";
+
+const dummyHouses = [
+  {
+    id: 1,
+    name: "House 1",
+    address: "123 Fake Street",
+    bedrooms: 3,
+    bathrooms: 2,
+    price: "$1000000",
+    image: defaultHouse,
+  },
+  {
+    id: 2,
+    name: "House 2",
+    address: "456 Fake Street",
+    bedrooms: 4,
+    bathrooms: 3,
+    price: "$2000000",
+    image: defaultHouse,
+  },
+  {
+    id: 3,
+    name: "House 3",
+    address: "789 Fake Street",
+    bedrooms: 5,
+    bathrooms: 4,
+    price: "$3000000",
+    image: defaultHouse,
+  },
+  {
+    id: 1,
+    name: "House 1",
+    address: "123 Fake Street",
+    bedrooms: 3,
+    bathrooms: 2,
+    price: "$1000000",
+    image: defaultHouse,
+  },
+  {
+    id: 2,
+    name: "House 2",
+    address: "456 Fake Street",
+    bedrooms: 4,
+    bathrooms: 3,
+    price: "$2000000",
+    image: defaultHouse,
+  },
+  {
+    id: 3,
+    name: "House 3",
+    address: "789 Fake Street",
+    bedrooms: 5,
+    bathrooms: 4,
+    price: "$3000000",
+    image: defaultHouse,
+  },
+];
 
 const Homepage = () => {
-  const dispatch = useDispatch()
-  const houses = useSelector((state) => state.houses)
-  console.log(houses)
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.houses);
+  console.log(data);
 
   useEffect(() => {
-    dispatch(fetchHouses())
-  },[dispatch])
+    dispatch(fetchHouses());
+  }, [dispatch]);
 
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="houses-container">
+      <div className="heading">
+        <h3>Our Houses</h3>
+        <small>Select a house to see more details</small>
+      </div>
+      <section className="houses-list">
+        {dummyHouses.map((house) => (
+          <HouseCard house={house} />
+        ))}
+      </section>
+      <HorizontalScroll />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
+
+const HouseCard = ({ house }) => {
+  return (
+    <>
+      <div key={house.id} className="screen">
+        <section className="house-image-container">
+          <img src={house.image} alt={house.name} className="house-image" />
+        </section>
+        <h4 className="house-name">{house.name}</h4>
+        <div className="divider" />
+        <p>{house.address}</p>
+        <section className="socials">
+          <MediumIcon />
+          <TwitterIcon />
+          <LinkedInIcon />
+        </section>
+      </div>
+    </>
+  );
+};
+
+const HorizontalScroll = () => {
+  const containerRef = useRef(null);
+
+  const handleScroll = (scrollOffset) => {
+    containerRef.current.scrollLeft += scrollOffset;
+  };
+
+  return (
+    <div className="horizontal-screen-view">
+      <div className="container" ref={containerRef}>
+        {/* Place your screen components here */}
+        <div className="screen screen1">Screen 1</div>
+        <div className="screen screen2">Screen 2</div>
+        <div className="screen screen3">Screen 3</div>
+        {dummyHouses.map((house) => (
+          <HouseCard house={house} />
+        ))}
+        {/* Add more screens as needed */}
+      </div>
+      <div className="scroll-buttons">
+        <button onClick={() => handleScroll(-200)}>&lt; Prev</button>
+        <button onClick={() => handleScroll(200)}>Next &gt;</button>
+      </div>
+    </div>
+  );
+};
