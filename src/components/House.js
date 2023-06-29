80fde33fb95779c14b8efe53fb1fef601ad3b15d
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-29 17:58:07 +0300

    Update house component

--- src/components/House.js
+++ src/components/House.js
@@ -1,6 +1,10 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { fetchHouse } from '../store/HouseSlice'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchHouse } from '../store/HouseSlice';
+import './House.css';
+import NavBar from '../nav/NavBar';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const HousePage = () => {
   const dispatch = useDispatch()
@@ -12,15 +16,61 @@ const HousePage = () => {
   }, [dispatch])
 
   return (
-    <div>
-      <h1>House Details</h1>
-      <img src={house.image} alt={house.name}/>
-      <p>Name: {house.name}</p>
-      <p>Location: {house.location}</p>
-      <p>Bedrooms: {house.bedrooms}</p>
-      <p>Bathrooms: {house.bathrooms}</p>
-      <p>Price: {house.price}</p>
-    </div>
+    <section className='container'>
+      {/* <NavBar /> */}
+      <div className="house-container d-flex flex-md-row flex-column">
+        <div className='img-container d-flex justify-content-center align-items-center m-auto'>
+          <img src={house.image} alt={house.name} className='img-fluid rounded mx-auto d-block'/>
+        </div>
+        <div className='info-container'>
+          <h1 className='d-flex justify-content-end pe-1'>{house.name}</h1>
+          <table className='table table-striped'>
+            <tbody>
+              <tr>
+                <td>
+                  <div className="d-flex justify-content-between">
+                    <span className='fw-bold'>Location:</span>
+                    <span>{house.location}</span>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="d-flex justify-content-between">
+                    <span className='fw-bold'>Bedrooms:</span>
+                    <span>{house.bedrooms}</span>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="d-flex justify-content-between">
+                    <span className='fw-bold'>Bathrooms:</span>
+                    <span>{house.bathrooms}</span>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="d-flex justify-content-between">
+                    <span className='fw-bold'>Price:</span>
+                    <span>${house.price}</span>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div>
+            <Link
+              to={`/reservations/new/${house.id}`}
+              className='res-link text-decoration-none d-flex justify-content-end'>
+              <button type="button" className="reserve-btn btn text-white fw-bold" style={{ backgroundColor: '#CCFF00' }}>RESERVE HOUSE</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+  </section>
+
   )
 }
 
