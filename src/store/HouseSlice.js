bb8d67d6c21039fba22ca7079c4d0fa9f1e004f6
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-30 13:14:30 +0300

    Update store files

--- src/store/HouseSlice.js
+++ src/store/HouseSlice.js
@@ -2,8 +2,8 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const baseUrl = 'http://127.0.0.1:3000/api/v1/houses';
 
-export const fetchHouse = createAsyncThunk("house/fetchHouse", async (id) => {
-  const url = `${baseUrl}/${3}`;
+export const fetchHouse = createAsyncThunk('house/fetchHouse', async (id) => {
+  const url = `${baseUrl}/${id}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
