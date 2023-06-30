7bb85f18ee95b004fd3c2fc8937a5b18a5fe2330
LeslieAine <aineleslie@gmail.com>
2023-06-30 22:59:18 +0300

    Fix linter errors

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,12 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import Homepage from '../components/Homepage';
 import HousePage from '../components/House';
 // import House from '../pages/House';
+import Reserve from '../pages/Reserve/Reserve';
 
 const AppRouter = () => (
   <Routes>
     <Route exact path="/" element={<Homepage />} />
     <Route path="/houses" element={HousePage} />
-    <Route path="/reserve" element={<h1>Reserve House</h1>} />
+    <Route path="/reserve" element={Reserve} />
     <Route path="/my-reservations" element={<h1>My Reservations</h1>} />
     <Route path="/add" element={HousePage} />
     <Route path="/delete" element={<h1>Delete House jdjdjdjj</h1>} />
