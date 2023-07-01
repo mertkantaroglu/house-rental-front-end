59e83f722a390e0405378435e225e22086354fb5
Zeph180 <zephrichards1@gmail.com>
2023-07-01 09:41:17 +0300

    Add reservations slice

--- src/pages/ReservationsList.js
+++ src/pages/ReservationsList.js
@@ -1,36 +1,20 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaTrashAlt } from 'react-icons/fa';
 import './ReservationsList.css';
-
-const reservations = [
-  {
-    id: 1,
-    name: 'House 1',
-    address: 'Address 1',
-    startDate: '2021-09-01',
-    endDate: '2021-09-10',
-  },
-  {
-    id: 2,
-    name: 'House 2',
-    address: 'Address 2',
-    startDate: '2021-09-01',
-    endDate: '2021-09-10',
-  },
-  {
-    id: 3,
-    name: 'House 3',
-    address: 'Address 3',
-    startDate: '2020-09-05',
-    endDate: '2020-09-10',
-  },
-];
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchReservations } from '../store/ReservationsSlice';
 
 const ReservationsList = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.reservations);
+
+  useEffect(() => {
+    dispatch(fetchReservations());
+  }, [dispatch]);
 
   const deleteReservation = () => {
     // implement delete reservation
@@ -52,12 +36,12 @@ const ReservationsList = () => {
           <span className="delete">Delete</span>
         </li>
 
-        {reservations.map((reservation) => (
+        {data.reservations.map((reservation) => (
           <li key={reservation.id} className="reservations-item">
             <span onClick={() => navigate(`/house/${reservation.id}`)} className="reservation-name">{reservation.name}</span>
-            <span>{reservation.address}</span>
-            <span className="reservation-start-date">{reservation.startDate}</span>
-            <span className="reservation-end-date">{reservation.endDate}</span>
+            <span>{reservation.city}</span>
+            <span className="reservation-start-date">{reservation.start_date}</span>
+            <span className="reservation-end-date">{reservation.end_date}</span>
             <span type="button"><FaTrashAlt onClick={() => deleteReservation(reservation)} className="delete-btn" /></span>
           </li>
         ))}
