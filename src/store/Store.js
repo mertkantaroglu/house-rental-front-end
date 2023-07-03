c4d52283495ab5740d8e5d88ebafb0836f40fa08
LeslieAine <aineleslie@gmail.com>
2023-07-03 23:59:52 +0300

    Update reducers

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import houseReducer from './HouseSlice';
 import housesReducer from './HousesSlice';
 import reservationsReducer from './ReservationsSlice';
+import authReducer from './AuthenticationSlice';
 
 export default configureStore({
   reducer: {
     house: houseReducer,
     houses: housesReducer,
     reservations: reservationsReducer,
+    authentication: authReducer
   },
 });
