a84bf3248ff51aad2b732d7003d13d3b6a108041
LeslieAine <aineleslie@gmail.com>
2023-07-03 23:58:43 +0300

    Add redux to the reservation and houses

--- src/store/HouseSlice.js
+++ src/store/HouseSlice.js
@@ -1,33 +1,224 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+const SHOW_HOUSES = 'house-rental/house/SHOW';
+const SHOW_HOUSE = 'house-rental/house/SHOW/:id';
+const ADD_HOUSE = 'house-rental/house/ADD';
+const DELETE_HOUSE = 'house-rental/house/DELETE';
 
-const baseUrl = 'http://127.0.0.1:3000/api/v1/houses';
+// const baseUrl = 'http://127.0.0.1:3000/api/v1/houses';
 
-export const fetchHouse = createAsyncThunk('house/fetchHouse', async (id) => {
-  const url = `${baseUrl}/${id}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+// export const fetchHouse = createAsyncThunk('house/fetchHouse', async (id) => {
+//   const url = `${baseUrl}/${id}`;
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   return data;
+// });
+
+
+
+// const houseSlice = createSlice({
+//   name: 'houses',
+//   initialState: {
+//     loading: false,
+//     house: {},
+//   },
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchHouse.pending, (state) => {
+//         const newState = state;
+//         newState.loading = true;
+//       })
+//       .addCase(fetchHouse.fulfilled, (state, action) => {
+//         const newState = state;
+//         newState.loading = false;
+//         newState.house = action.payload;
+//       });
+//   },
+// });
+
+// export default houseSlice.reducer;
+
+
+// Base Url
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+// Method gethouses
+export const getHouses = createAsyncThunk(SHOW_HOUSES, async (filter = null, thunkAPI) => {
+  const API_URL = filter === true ? `${BASE_URL}/api/v1/houses?filter=true` : `${BASE_URL}/api/v1/houses`;
+  const token = localStorage.getItem('token');
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    return await axios.get(API_URL, requestOptions);
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response.data.error);
+  }
 });
 
+export const getHouse = createAsyncThunk(SHOW_HOUSE, async (id, thunkAPI) => {
+  const API_URL = `${BASE_URL}/api/v1/houses/${id}`;
+  const token = localStorage.getItem('token');
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    return await axios.get(API_URL, requestOptions);
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response.data.error);
+  }
+});
+
+// Method Addhouse
+export const addHouse = createAsyncThunk(ADD_HOUSE, async (house, thunkAPI) => {
+  const API_URL = `${BASE_URL}/api/v1/houses`;
+  const token = localStorage.getItem('token');
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    return await axios.post(API_URL, house, requestOptions);
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response.data.error);
+  }
+});
+
+// Method Delete house
+export const deleteHouse = createAsyncThunk(DELETE_HOUSE, async (id, thunkAPI) => {
+  const API_URL = `${BASE_URL}/api/v1/houses/${id}`;
+  const token = localStorage.getItem('token');
+  const requestOptions = {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    await axios.delete(API_URL, requestOptions);
+    return id;
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response.data.error);
+  }
+});
+
+// house Slice
 const houseSlice = createSlice({
-  name: 'houses',
+  name: 'house',
   initialState: {
-    loading: false,
-    house: {},
+    isLoading: false,
+    success: false,
+    error: '',
+    list: [],
+    house: null,
+    response: null,
   },
-  reducers: {},
+  reducers: {
+    resetErrors: (state) => ({
+      ...state,
+      error: '',
+      isLoading: false,
+      success: false,
+      response: null,
+    }),
+  },
   extraReducers: (builder) => {
-    builder
-      .addCase(fetchHouse.pending, (state) => {
-        const newState = state;
-        newState.loading = true;
-      })
-      .addCase(fetchHouse.fulfilled, (state, action) => {
-        const newState = state;
-        newState.loading = false;
-        newState.house = action.payload;
-      });
+    // Get houses
+    builder.addCase(getHouses.pending, (state) => ({
+      ...state,
+      isLoading: true,
+      error: '',
+    }));
+
+    builder.addCase(getHouses.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: false,
+      success: true,
+      list: action.payload.data.data.houses,
+    }));
+
+    builder.addCase(getHouses.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload,
+    }));
+
+    // Get house
+    builder.addCase(getHouse.pending, (state) => ({
+      ...state,
+      isLoading: true,
+      error: '',
+    }));
+
+    builder.addCase(getHouse.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: false,
+      success: true,
+      house: action.payload.data.data.houses,
+    }));
+
+    builder.addCase(getHouse.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload,
+    }));
+
+    // Add house
+
+    builder.addCase(addHouse.pending, (state) => ({
+      ...state,
+      isLoading: true,
+      error: '',
+    }));
+
+    builder.addCase(addHouse.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: false,
+      success: true,
+      response: action.payload.data.data,
+    }));
+
+    builder.addCase(addHouse.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      errors: action.payload.data.errors,
+    }));
+
+    // Delete house
+
+    builder.addCase(deleteHouse.pending, (state) => ({
+      ...state,
+      isLoading: true,
+      error: '',
+    }));
+
+    builder.addCase(deleteHouse.fulfilled, (state, action) => {
+      const id = action.payload;
+
+      return {
+        ...state,
+        isLoading: false,
+        success: true,
+        id,
+        list: state.list.filter((house) => house.id !== id),
+      };
+    });
+
+    builder.addCase(deleteHouse.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload.data.error,
+    }));
   },
 });
 
 export default houseSlice.reducer;
+export const { resetErrors } = houseSlice.actions;
--- src/store/ReservationsSlice.js
+++ src/store/ReservationsSlice.js
@@ -1,30 +1,165 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-export const fetchReservations = createAsyncThunk('reservations/fetchReservations', async () => {
-  const response = await fetch('http://127.0.0.1:3000/api/v1/reservations');
-  const data = response.json();
-  return data;
+// export const fetchReservations = createAsyncThunk('reservations/fetchReservations', async () => {
+//   const response = await fetch('http://127.0.0.1:3000/api/v1/reservations');
+//   const data = response.json();
+//   return data;
+// });
+
+// const reservationsSlice = createSlice({
+//   name: 'reservations',
+//   initialState: {
+//     loading: false,
+//     reservations: [],
+//   },
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchReservations.pending, (state) => {
+//         const newState = state;
+//         newState.loading = true;
+//       })
+//       .addCase(fetchReservations.fulfilled, (state, action) => {
+//         const newState = state;
+//         newState.loading = false;
+//         newState.reservations = action.payload;
+//       });
+//   },
+// });
+
+// export default reservationsSlice.reducer;
+
+
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const SHOW_RESERVATIONS = 'house-rental/reservation/SHOW';
+const ADD_RESERVATION = 'house-rental/reservation/ADD';
+const DELETE_RESERVATION = 'house-rental/reservation/DELETE';
+
+// Method getReservations
+export const getReservations = createAsyncThunk(SHOW_RESERVATIONS, async (thunkAPI) => {
+  const API_URL = 'http://localhost:3000/api/v1/reservations';
+  const token = localStorage.getItem('token');
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    return await axios.get(API_URL, requestOptions);
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response.data.error);
+  }
 });
 
-const reservationsSlice = createSlice({
-  name: 'reservations',
+// Method AddReservation
+export const addReservation = createAsyncThunk(ADD_RESERVATION, async (reservation, thunkAPI) => {
+  const API_URL = 'http://localhost:3000/api/v1/reservations';
+  const token = localStorage.getItem('token');
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    return await axios.post(API_URL, reservation, requestOptions);
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response.data.error);
+  }
+});
+
+// Method DeleteReservation
+export const deleteReservation = createAsyncThunk(DELETE_RESERVATION, async (id, thunkAPI) => {
+  const API_URL = `http://localhost:3000/api/v1/reservations/${id}`;
+  const token = localStorage.getItem('token');
+  const requestOptions = {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    return await axios.delete(API_URL, requestOptions);
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response.data.error);
+  }
+});
+
+// Reservation Slice
+const reservationSlice = createSlice({
+  name: 'reservation',
   initialState: {
-    loading: false,
-    reservations: [],
+    isLoading: false,
+    success: false,
+    error: '',
+    list: '',
   },
-  reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(fetchReservations.pending, (state) => {
-        const newState = state;
-        newState.loading = true;
-      })
-      .addCase(fetchReservations.fulfilled, (state, action) => {
-        const newState = state;
-        newState.loading = false;
-        newState.reservations = action.payload;
-      });
+    // Get Reservations
+    builder.addCase(getReservations.pending, (state) => ({
+      ...state,
+      isLoading: true,
+      error: '',
+    }));
+
+    builder.addCase(getReservations.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: false,
+      success: true,
+      list: action.payload.data.data.reservations,
+    }));
+
+    builder.addCase(getReservations.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload,
+    }));
+
+    // Add Reservation
+
+    builder.addCase(addReservation.pending, (state) => ({
+      ...state,
+      isLoading: true,
+      error: '',
+    }));
+
+    builder.addCase(addReservation.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: false,
+      success: true,
+      response: action.payload.data.data,
+    }));
+
+    builder.addCase(addReservation.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      errors: action.payload.data.errors,
+    }));
+
+    // Delete Reservation
+
+    builder.addCase(deleteReservation.pending, (state) => ({
+      ...state,
+      isLoading: true,
+      error: '',
+    }));
+
+    builder.addCase(deleteReservation.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: false,
+      success: true,
+      id: action.payload.data.data,
+    }));
+
+    builder.addCase(deleteReservation.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload.data.error,
+    }));
   },
 });
 
-export default reservationsSlice.reducer;
+export default reservationSlice.reducer;
