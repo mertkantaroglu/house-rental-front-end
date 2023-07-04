9d60412f364f03fb131c88f578c7e725bd30193f
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-07-04 11:28:44 +0300

    Update landing page

--- src/pages/landingPage/Landing.js
+++ src/pages/landingPage/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignupForm from '../../components/Authentication/SignupForm';
+import LoginForm from '../../components/Authentication/LoginForm';
 import './LandingPage.css';
 
 const LandingPage = () => (
   <div className="landing-page">
     <div className="landing-page-content">
+      <LoginForm />
+      <SignupForm />
       <h1 className="landing-page-title">Every house has the ability to inspire!</h1>
       <Link to="login" className="landing-page-btn">Get started</Link>
     </div>
