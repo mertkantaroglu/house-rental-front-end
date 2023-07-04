72d3bfd800210f58683974dba1004c8b62b42e3c
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-07-04 11:30:19 +0300

    Create login form

---
+++ src/components/Authentication/LoginForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { loginUser } from '../../store/AuthenticationSlice';
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, isLoading, error } = useSelector((state) => state.authentication);
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(loginUser({ user: credentials })).then(() => {
+      if (user) {
+        navigate('/home');
+      }
+    });
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={credentials.email}
+        onChange={handleInputChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={credentials.password}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Login</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
+};
+
+export default LoginForm;
