afce2aa2e6ddf9c24bc49ff8c0c5982f6742fc2b
Zeph180 <zephrichards1@gmail.com>
2023-07-06 14:41:30 +0300

    Fix linters

--- src/pages/AddHouse/AddHouse.jsx
+++ src/pages/AddHouse/AddHouse.jsx
@@ -1,44 +1,49 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom'
-import './index.css';
-import Input from '../../components/Input/Input';
-import FormErrors from '../../ui/FormErrors';
-import { addHouse } from '../../store/HouseSlice';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
+import Input from "../../components/Input/Input";
+import FormErrors from "../../ui/FormErrors";
+import { addHouse } from "../../store/HouseSlice";
 
 const AddHouse = () => {
   const formInfoState = {
-    name: '',
-    location: '',
-    bedrooms: '',
-    bathrooms: '',
-    price: '',
-    image: '',
+    name: "",
+    location: "",
+    bedrooms: "",
+    bathrooms: "",
+    price: "",
+    image: "",
     validations: {
       nameValid: null,
       priceValid: null,
       isValid: null,
       formErrors: {
-        name: '', price: '',
+        name: "",
+        price: "",
       },
     },
   };
   const [formInfo, setFormInfo] = useState(formInfoState);
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.authentication);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const validateField = (fieldName, fieldValue) => {
     const { formErrors } = formInfo.validations;
     let { nameValid, priceValid } = formInfo.validations;
     switch (fieldName) {
-      case 'name':
+      case "name":
         nameValid = fieldValue.length >= 3;
-        formErrors.name = nameValid ? '' : ' should contain atleast 3 characters ';
+        formErrors.name = nameValid
+          ? ""
+          : " should contain atleast 3 characters ";
         break;
-      case 'price':
+      case "price":
         priceValid = !Number.isNaN(fieldValue) && fieldValue > 0;
-        formErrors.price = priceValid ? '' : ' should be a number greater than 0';
+        formErrors.price = priceValid
+          ? ""
+          : " should be a number greater than 0";
         break;
       default:
         break;
@@ -63,34 +68,60 @@ const AddHouse = () => {
     const newhouse = { ...formInfo, user_id: user.status.data.id };
     delete newhouse.validations;
     dispatch(addHouse(newhouse));
-    navigate('/houses')
-    setFormInfo(
-      formInfoState,
-    );
+    navigate("/houses");
+    setFormInfo(formInfoState);
   };
 
   const houseForm = () => (
     <>
       <h2>Add A house</h2>
-      <FormErrors
-        formErrors={formInfo.validations.formErrors}
-      />
+      <FormErrors formErrors={formInfo.validations.formErrors} />
       <form onSubmit={handleSubmit}>
-        <Input name="name" type="text" onInput={handleInput} value={formInfo.name} isValid={formInfo.validations.nameValid} />
-        <Input name="location" type="text" onInput={handleInput} value={formInfo.location} />
-        <Input name="bedrooms" type="text" onInput={handleInput} value={formInfo.bedrooms} />
-        <Input name="bathrooms" type="number" onInput={handleInput} value={formInfo.bathrooms} />
-        <Input name="price" type="number" onInput={handleInput} value={formInfo.price} isValid={formInfo.validations.priceValid} />
-        <Input name="image" type="text" onInput={handleInput} value={formInfo.image} />
+        <Input
+          name="name"
+          type="text"
+          onInput={handleInput}
+          value={formInfo.name}
+          isValid={formInfo.validations.nameValid}
+        />
+        <Input
+          name="location"
+          type="text"
+          onInput={handleInput}
+          value={formInfo.location}
+        />
+        <Input
+          name="bedrooms"
+          type="text"
+          onInput={handleInput}
+          value={formInfo.bedrooms}
+        />
+        <Input
+          name="bathrooms"
+          type="number"
+          onInput={handleInput}
+          value={formInfo.bathrooms}
+        />
+        <Input
+          name="price"
+          type="number"
+          onInput={handleInput}
+          value={formInfo.price}
+          isValid={formInfo.validations.priceValid}
+        />
+        <Input
+          name="image"
+          type="text"
+          onInput={handleInput}
+          value={formInfo.image}
+        />
         <input type="submit" value="Submit" />
       </form>
     </>
   );
   return (
     <section id="add_house">
-      <div className="form__container">
-        { houseForm() }
-      </div>
+      <div className="form__container">{houseForm()}</div>
     </section>
   );
 };
